6e8987f199095dd4e6650b03f4d561a6de153212
MelaniaPicone <melania_picone@yahoo.it>
2025-07-21 12:41:25 +0200

    [feat] absolute pin

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ for (let i=0; i<6 ; i++){
 postCards.innerHTML += `
 <div class="col-sm-12 col-md-6 col-lg-4 mt-4 mb-3 ">
 <div class="card p-1 border-shadow" style="width: 16rem;">
+  <img src="./assets/img/pin.svg" class="pin">
   <img src="${travelCards[i].url}" class="img img-fluid" alt="immagine">
   <div class="card-body" style="height:110px;"">
   <div class="id"><h5>${travelCards[i].id}</h5></div>
